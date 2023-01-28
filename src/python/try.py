6779514a44df50592b1e4c748cf59a72268554e6
adaliuBC <adaliu1998@163.com>
2023-01-27 19:25:10 -0800

    Have phong model with wrong light - light moving with the model rotating

--- src/python/try.py
+++ src/python/try.py
@@ -27,41 +27,65 @@ class MyCanvas(Canvas):
         try:
             import numpy as np
 
-            if nanogui.api == 'opengl':
-                vertex_shader = """
-                    #version 330
-                    uniform mat4 mvp;
-                    in vec3 position;
-                    in vec3 color;
-                    out vec4 frag_color;
-                    //撒旦书
+            vertex_shader = """
+                #version 330
+                uniform mat4 model_mat;
+                uniform mat4 view_mat;
+                uniform mat4 projection_mat;
+                in vec3 position;
+                in vec3 color;
+                out vec4 frag_color;
+                in vec3 normal;
+
+                // for reflection model
+                out vec3 frag_position;
+                out vec3 frag_normal;
+
+                void main() {
+                    frag_color = vec4(color, 1.0);
+                    mat4 mvp = projection_mat * view_mat * model_mat; 
+                    gl_Position = mvp * vec4(position, 1.0);
+                    frag_position = position;
+                    frag_normal = normal;
+                }
+                """
+
+            fragment_shader = """
+                #version 330
+                out vec4 color;
+                in vec4 frag_color;
+
+                // for Phong
+                in vec3 frag_position;
+                in vec3 frag_normal;
+                uniform vec3 light_position;
+                uniform float light_intensity;
+                uniform vec4 light_color;
+                uniform vec3 view_position;
+
+                void main() {
+                    // ambient
+                    float ambient = 0.3;
                     
+                    // diffuse
+                    float kd = 5.0;
+                    float distance = length(light_position - frag_position);
+                    vec3 light_dir = normalize(light_position - frag_position);
+                    vec3 norm = normalize(frag_normal);
+                    float energy_diffuse = max(dot(norm, light_dir), 0.0);
+                    float diffuse = kd * light_intensity / (distance * distance) * energy_diffuse;
 
-                    void main() {
-                        frag_color = vec4(color, 1.0);
-                        gl_Position = mvp * vec4(position, 1.0);
-                    }
-                    """
+                    //specular
+                    float ks = 3.0;
+                    vec3 view_dir = normalize(view_position - frag_position);
+                    vec3 h = (view_dir + light_dir) / length(view_dir + light_dir);
+                    float p = 32;
+                    float energy_specular = pow( max(dot(norm, h), 0.0), p);
+                    float specular = ks * energy_specular;
 
-                fragment_shader = """
-                    #version 330
-                    uniform vec4 light_position;
-                    uniform vec4 light_color;
-                    out vec4 color;
-                    in vec4 frag_color;
-                    void main() {
-                        // ambient
-                        float ks = 0.3;
-                        
-                        // diffuse
-                        float kd = 0.3;
-                        vec4 distance = light_position;
-                        vec4 distance2 = frag_color;
-                        color =  (kd) * light_color * frag_color;
-                    }
-                    """
-            else:
-                raise Exception("Unknown graphics API!")
+                    color =  (ambient + diffuse + specular) * light_color * frag_color;
+                }
+                """
 
             self.shader = Shader(
                 self.render_pass(),
@@ -89,6 +113,13 @@ class MyCanvas(Canvas):
                  [1, -1, -1], [1, 1, -1]],
                 dtype=np.float32)
 
+            normals = np.array(
+                [[-1, 1, 1], [-1, -1, 1],
+                 [1, -1, 1], [1, 1, 1],
+                 [-1, 1, -1], [-1, -1, -1],
+                 [1, -1, -1], [1, 1, -1]],
+                dtype=np.float32)
+
             colors = np.array(
                 [[0, 1, 1], [0, 0, 1],
                  [1, 0, 1], [1, 1, 1],
@@ -98,13 +129,18 @@ class MyCanvas(Canvas):
                 
             light_color = np.array([1, 1, 1, 1], dtype=np.float32)
             
-            light_position = np.array([1.5, 1.5, 1.5, 1], dtype = np.float32)
-
+            light_position = np.array([0, 0., 5], dtype = np.float32)
+            light_intensity = np.array(1, dtype = np.float32)
+            view_position = np.array([0, 0, 3], dtype = np.float32)
             self.shader.set_buffer("indices", indices)
             self.shader.set_buffer("position", positions)
             self.shader.set_buffer("color", colors)
             self.shader.set_buffer("light_color", light_color)
             self.shader.set_buffer("light_position", light_position)
+            self.shader.set_buffer("light_intensity", light_intensity)
+            self.shader.set_buffer("normal", normals)
+            self.shader.set_buffer("view_position", view_position)
+            
             self.rotation = 0
         except ImportError:
             self.shader = None
@@ -141,7 +177,9 @@ class MyCanvas(Canvas):
 
         mvp = proj @ view @ model @ model2
 
-        self.shader.set_buffer("mvp", mvp.T)
+        self.shader.set_buffer("model_mat", (model @ model2).T)
+        self.shader.set_buffer("view_mat", view.T)
+        self.shader.set_buffer("projection_mat", proj.T)
         with self.shader:
             self.shader.draw_array(Shader.PrimitiveType.Triangle,
                                    0, 36, indexed=True)
